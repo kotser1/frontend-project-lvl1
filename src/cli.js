3a79c134c3ef7b2fab32470cbc809436d93e34a3
kotser1 <newelvin@gmail.com>
2021-11-21 18:23:05 +0300

    step 3

---
+++ src/cli.js
@@ -0,0 +1,8 @@
+import readlineSync from 'readline-sync';
+
+const cli = (question) => {
+  const name = readlineSync.question(question);
+  console.log('Hello, ', name, '!');
+};
+
+export { cli };
